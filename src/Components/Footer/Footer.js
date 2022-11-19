5a008b2b3d3fdbfef10c75f0699e49b99cffeec8
Umang2802 <umangmetri@gmail.com>
2022-11-19 13:00:05 +0530

    fixed footer arrow

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -33,13 +33,15 @@ export const Footer = () => {
             <Contact>
               Lisitng 1:{" "}
               <a href="/">
-                Room 1 <NearMeIcon sx={{ color: "black" }}></NearMeIcon>
+                Room 1{" "}
+                <NearMeIcon fontSize="sm" sx={{ color: "black" }}></NearMeIcon>
               </a>
             </Contact>
             <Contact>
               Lisitng 2:{" "}
               <a href="/">
-                Room 2 <NearMeIcon sx={{ color: "black" }}></NearMeIcon>
+                Room 2{" "}
+                <NearMeIcon fontSize="sm" sx={{ color: "black" }}></NearMeIcon>
               </a>
             </Contact>
           </Column>
